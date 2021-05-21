3b1d968d5f4f713d29c40e0071c73e89b376be8b
Bjarte Aarmo Lund <bjarte.lund@gmail.com>
2021-05-21 13:27:11 +0200

    Update collect-arrhenius.sh
    Add a column with the simulations temperature

--- collect-arrhenius.sh
+++ collect-arrhenius.sh
@@ -3,4 +3,5 @@ for temp in  273 278 283 288 293 298 303 308
 do
         echo $temp
 sed -n '/------------------------------- Free energies ---------------------------------/,${p;/----------------------------------- SUMMARY -----------------------------------/q}'  $temp/q_analysefeps.log |head -n -2|tail -n +3 > $temp.csv
+sed -i "s/$/\t$temp/" $temp.csv 
 done
