a0e0bf8bc02f4cbb17120ed2d8a4498bb06b5435
Bjarte Aarmo Lund <bjarte.lund@gmail.com>
2021-05-21 13:14:51 +0200

    Create collect-arrhenius.sh

---
+++ collect-arrhenius.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+for temp in  273 278 283 288 293 298 303 308
+do
+        echo $temp
+sed -n '/------------------------------- Free energies ---------------------------------/,${p;/----------------------------------- SUMMARY -----------------------------------/q}'  $temp/q_analysefeps.log |head -n -2|tail -n +3 > $temp.csv
+done
